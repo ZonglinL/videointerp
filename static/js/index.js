394ef2441a1ade9d24a3998027c6974fc2a60066
ZonglinL <97364054+ZonglinL@users.noreply.github.com>
2024-04-17 15:22:50 -0400

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
     // Check for click events on the navbar burger icon
 
     var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
+			slidesToScroll: 0,
+			slidesToShow: 0,
 			loop: true,
 			infinite: true,
 			autoplay: false,
